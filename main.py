b5f348af96ff434c2119907639f429863dfd8ef8
Kevdev971 <punone971@gmail.com>
2025-03-09 04:48:40 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 LOCAL = [
-    400,300,
-    "Test game 0.1"
+    800,600,
+    "Test game 0.2"
 ]
+speed = 10
+playerX = 400
+playerY = 300
 pygame.init()
 pygame.display.set_caption(LOCAL[2])
-pygame.display.set_mode((LOCAL[0],LOCAL[1]))
-
+screen = pygame.display.set_mode((LOCAL[0],LOCAL[1]))
+screen_rect = screen.get_rect()
+pygame.time.Clock().tick(60)
+player = pygame.image.load("image/player.png")
+Player = player.get_rect()
 run = True
+
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.KEYUP:
-            print("KEY")
-        if event.type == pygame.KEYDOWN:
-            print("KEY")
         if event.type == pygame.QUIT:
-            pygame.quit()
             run = False
-            exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        Player.y -= speed
+
+    if keys[pygame.K_DOWN]:
+        Player.y += speed
+    
+    if keys[pygame.K_LEFT]:
+        Player.x -= speed
+
+    if keys[pygame.K_RIGHT]:
+        Player.x += speed
+
+    screen.blit(player,Player)
+    pygame.display.update()
+pygame.quit()
